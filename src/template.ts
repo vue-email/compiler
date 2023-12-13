5edb728a46fd233d68e6cee5051920f6af00a0ad
Younes Barrad <younesbarraddev@gmail.com>
2023-12-13 19:53:21 +0100

    chore: added trycatch to templateRender

--- src/template.ts
+++ src/template.ts
@@ -30,77 +30,83 @@ const components = {
 }
 
 export async function templateRender(name: string, code: SourceOptions, options?: RenderOptions, config?: Options): Promise<string> {
-  let vueI18n
+  try {
+    let vueI18n
 
-  const verbose = config?.verbose || false
-  const hasI18n = options?.i18n?.defaultLocale || config?.options?.i18n?.defaultLocale || options?.i18n?.translations || config?.options?.i18n?.translations
+    const verbose = config?.verbose || false
+    const hasI18n = options?.i18n?.defaultLocale || config?.options?.i18n?.defaultLocale || options?.i18n?.translations || config?.options?.i18n?.translations
 
-  const props = options?.props || config?.options?.props
-  name = correctName(name)
-  const component = await loadComponent(name, code.source, verbose)
+    const props = options?.props || config?.options?.props
+    name = correctName(name)
+    const component = await loadComponent(name, code.source, verbose)
 
-  if (verbose)
-    console.warn(`${lightGreen('💌')} ${bold(blue('Generating output'))}`)
+    if (verbose)
+      console.warn(`${lightGreen('💌')} ${bold(blue('Generating output'))}`)
 
-  if (!component)
-    throw new Error(`Component ${name} not found`)
+    if (!component)
+      throw new Error(`Component ${name} not found`)
 
-  const app = createApp(component, props)
-  app.use(VueEmailPlugin, config?.options)
-  app.config.performance = true
+    const app = createApp(component, props)
+    app.use(VueEmailPlugin, config?.options)
+    app.config.performance = true
 
-  if (code.components && code.components.length > 0) {
-    for (const emailComponent of code.components) {
-      const componentName = correctName(emailComponent.name)
-      const componentCode = await loadComponent(componentName, emailComponent.source, verbose)
-      if (componentCode) {
-        app.component(componentName, {
-          ...componentCode,
-          components,
-        })
+    if (code.components && code.components.length > 0) {
+      for (const emailComponent of code.components) {
+        const componentName = correctName(emailComponent.name)
+        const componentCode = await loadComponent(componentName, emailComponent.source, verbose)
+        if (componentCode) {
+          app.component(componentName, {
+            ...componentCode,
+            components,
+          })
+        }
       }
     }
+
+    if (hasI18n) {
+      const i18nOptions: I18n = {
+        defaultLocale: options?.i18n?.defaultLocale || config?.options?.i18n?.defaultLocale || 'en',
+        translations: options?.i18n?.translations || config?.options?.i18n?.translations,
+      }
+
+      try {
+        vueI18n = await import('vue-i18n')
+      }
+      catch (error) {
+        throw new Error(`${lightGreen('❌')} ${bold(red(`Missing vue-i18n dependency`))} ${white('please install it using: ')} ${bold(white('npm i vue-i18n@9'))}`)
+      }
+
+      const locale = i18nOptions.defaultLocale
+      if (locale && vueI18n) {
+        if (verbose)
+          console.warn(`${lightGreen('🌎')} ${bold(blue('Injecting translations'))}`)
+
+        const i18n = vueI18n.createI18n({
+          locale,
+          fallbackLocale: i18nOptions.defaultLocale,
+          messages: i18nOptions.translations,
+          silentFallbackWarn: !verbose,
+          silentTranslationWarn: !verbose,
+          warnHtmlInMessage: 'off',
+        })
+
+        app.use(i18n)
+      }
+    }
+
+    if (verbose)
+      console.warn(`${lightGreen('🎉')} ${bold(blue('Rendering template'))} ${bold(lightGreen(name))}`)
+
+    const markup = await renderToString(app)
+    const doctype = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">'
+    const doc = `${doctype}${cleanup(markup)}`
+
+    return doc
   }
-
-  if (hasI18n) {
-    const i18nOptions: I18n = {
-      defaultLocale: options?.i18n?.defaultLocale || config?.options?.i18n?.defaultLocale || 'en',
-      translations: options?.i18n?.translations || config?.options?.i18n?.translations,
-    }
-
-    try {
-      vueI18n = await import('vue-i18n')
-    }
-    catch (error) {
-      throw new Error(`${lightGreen('❌')} ${bold(red(`Missing vue-i18n dependency`))} ${white('please install it using: ')} ${bold(white('npm i vue-i18n@9'))}`)
-    }
-
-    const locale = i18nOptions.defaultLocale
-    if (locale && vueI18n) {
-      if (verbose)
-        console.warn(`${lightGreen('🌎')} ${bold(blue('Injecting translations'))}`)
-
-      const i18n = vueI18n.createI18n({
-        locale,
-        fallbackLocale: i18nOptions.defaultLocale,
-        messages: i18nOptions.translations,
-        silentFallbackWarn: !verbose,
-        silentTranslationWarn: !verbose,
-        warnHtmlInMessage: 'off',
-      })
-
-      app.use(i18n)
-    }
+  catch (error) {
+    console.error('Error rendering template', error)
+    return ''
   }
-
-  if (verbose)
-    console.warn(`${lightGreen('🎉')} ${bold(blue('Rendering template'))} ${bold(lightGreen(name))}`)
-
-  const markup = await renderToString(app)
-  const doctype = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">'
-  const doc = `${doctype}${cleanup(markup)}`
-
-  return doc
 }
 
 function correctName(name: string) {
